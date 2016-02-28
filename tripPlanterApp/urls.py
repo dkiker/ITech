592a2a0c36ced65c4fdb0492e7934fdd0c29b626
Dimitrios <2014573k@student.gla.ac.uk>
2016-02-28 00:21:59 +0000

    Template Continued
    … Meaningful comment

--- tripPlanterApp/urls.py
+++ tripPlanterApp/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, url
 from tripPlanterApp import views
 
 urlpatterns = patterns('',
-                        url(r'^/$', views.index, name='index'),
+                        url(r'^$', views.index, name='index'),
                         url(r'^trip/(?P<tripID>[\w\-]+)/$',views.summary, name='tripSummary'),
+                        url(r'^plan/$',views.plan, name='trip'),
+                        url(r'^explore/$',views.explore, name='explore'),
                         url(r'^add_trip/', views.add_trip, name='add_trip'),
                        )
--- tripPlanterApp/views.py
+++ tripPlanterApp/views.py
@@ -4,17 +4,29 @@ from django.contrib.auth.models import User
 from forms import MyForm
 
 def index(request):
-
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-
+    context_dict ={}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
 
     return render(request, 'index.html', context_dict)
 
+
+def plan(request):
+    context_dict ={}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'plan.html', context_dict)
+
+def explore(request):
+    context_dict ={}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'explore.html', context_dict)
 
 def summary(request,tripID):
 
