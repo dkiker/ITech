1156e02a73a6b60a8328ba7bf1f6a36c0c5ad8ac
Skeftical <fotissavva@hotmail.com>
2016-03-23 00:40:36 +0000

    added test

---
+++ TripPlanter/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from tripPlanterApp.models import Place
+
+class PlaceTest(TestCase):
+
+    def test_ensure_prices_are_positive(self):
+
+        """
+                make sure the prices are positive
+        """
+        place = Place.objects.get_or_create(name="Ketchup Westend", lat=55.87453, lon=-4.293269, price=-15)[0]
+        place.type = 'R'
+        place.location = "Glasgow"
+        place.description = "Quirky venue with a vintage diner look and a fun vibe for kids, with dressing-up and party bags."
+        place.save()
+        self.assertEqual((place.price >= 0), True)
+
